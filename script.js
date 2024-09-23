7b45fa6b4d14d238ee3183f1e688ce87c6f21195
maitri112004 <157736746+maitri112004@users.noreply.github.com>
2024-09-22 21:04:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,26 @@
-// Step 1
+// Challenge 1
 
-const library = [
-    {
-      title: "Book One",
-      author: "Author One",
-      status: {
-        own: true,
-        reading: false,
-        read: false
-      }
-    },
-    {
-      title: "Book Two",
-      author: "Author Two",
-      status: {
-        own: true,
-        reading: false,
-        read: false
-      }
-    },
-    {
-      title: "Book Three",
-      author: "Author Three",
-      status: {
-        own: true,
-        reading: false,
-        read: false
-      }
-    }
-  ];
-  
-  // Step 2
-  library[0].status.read = true;
-  library[1].status.read = true;
-  library[2].status.read = true;
-  
-  // Step 3
-  const { title: firstBook } = library[0];
-  
-  // Step 4
-  const libraryJSON = JSON.stringify(library);
-  
-  console.log(firstBook); 
-  console.log(libraryJSON);
+
+const arr = [1, 2, 3, 4, 5];
+
+
+arr.reverse();
+
+
+arr.unshift(6);
+
+
+arr.push(0);
+
+console.log(arr);
+
+
+// Challenge 2
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+
+const arr3 = arr1.concat(arr2.slice(1));
+
+console.log(arr3);
